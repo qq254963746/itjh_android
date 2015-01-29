f7dcbe879a846f7d1578fd8083b48dc9eaf3d86c
itjhDev <github@itjh.com.cn>
2015-01-29 16:07:28 +0800

    设置背景颜色
    设置背景颜色

---
+++ src/cn/com/itjh/itjh_android/FirstActivity.java
@@ -0,0 +1,17 @@
+package cn.com.itjh.itjh_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Hu on 2015/1/29 0029.
+ */
+public class FirstActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+    }
+}
